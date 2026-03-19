748d31702e07dfb2176a28412f727151be5d8808
leojp04 <leojp04@gmail.com>
2026-03-18 22:14:41 -0300

    feat: add pagination and sorting to product list

--- src/main/java/br/com/fiap/api_rest/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/api_rest/controller/ProdutoController.java
@@ -6,6 +6,10 @@ import br.com.fiap.api_rest.model.Produto;
 import br.com.fiap.api_rest.service.ProdutoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -38,12 +42,13 @@ public class ProdutoController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ProdutoResponse>> readProduto() {
-        List<ProdutoResponse> produtos = produtoService.read();
+    public ResponseEntity<Page<ProdutoResponse>> readProduto() {
+        Pageable pageable = PageRequest.of(0, 2, Sort.by("nome").ascending());
+        Page<ProdutoResponse> produtos = produtoService.read(pageable);
         if (produtos.isEmpty()) {
             return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(produtoService.read(), HttpStatus.OK);
+        return new ResponseEntity<>(produtos, HttpStatus.OK);
     }
 
     @PutMapping
