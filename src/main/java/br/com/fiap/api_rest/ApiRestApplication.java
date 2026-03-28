ef554a084b3c953477a32f2d6259ae64d6c2ca6e
leojp04 <leojp04@gmail.com>
2026-03-27 22:12:22 -0300

    feat: Enable Swagger UI for API documentation and update project dependencies

--- src/main/java/br/com/fiap/api_rest/ApiRestApplication.java
+++ src/main/java/br/com/fiap/api_rest/ApiRestApplication.java
@@ -1,9 +1,14 @@
 package br.com.fiap.api_rest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "API de Produtos",
+		version = "v1"))
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
